bf29b152d3748ca6640a9367f05c46ac171c6d8c
armandrismir <adrismir@outlook.com>
2024-03-07 18:35:31 -0800

    run on local network not just device

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
 # gunicorn_config.py
 accesslog = "-"
 access_log_format = "%(h)s %(r)s %(s)s %(t)s %(b)s"
-bind = "localhost:8000"
+bind = "0.0.0.0:8000"
--- run.py
+++ run.py
@@ -4,8 +4,6 @@ app = create_app(False)
 
 if __name__ == '__main__':
     app.run(
-        host='localhost', 
-        port=8000, 
-        ssl_context=('/etc/letsencrypt/live/strawberry.ddns.net/fullchain.pem', 
-                     '/etc/letsencrypt/live/strawberry.ddns.net/privkey.pem')
+        host='0.0.0.0', 
+        port=8000,
         )
