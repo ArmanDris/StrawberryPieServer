c9347c928859a173224334ac8064967d3292fd0a
armandrismir <adrismir@outlook.com>
2024-03-07 18:31:45 -0800

    No longer accept args

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-import argparse
 from app import create_app
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--test', action='store_true', help='run with http locally on port 3001 and test database')
-args = parser.parse_args()
-
-app = create_app(test=args.test)
+app = create_app(False)
 
 if __name__ == '__main__':
     app.run(
